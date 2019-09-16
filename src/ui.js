9a02c66e6b50c2a86704184f5b8966509e31c0ed
Bart Simons <bart.simons@vicompany.nl>
2019-09-16 13:38:30 +0200

    Version 2 - Fix

--- src/ui.js
+++
@@ -1,9 +0,0 @@
-import './ui.css';
-// ToDo: Multiple elements selection
-document.getElementById('replace').onclick = () => {
-	const body = 'Replaced text string';
-	parent.postMessage({ pluginMessage: { type: 'cancel', body } }, '*');
-};
-document.getElementById('cancel').onclick = () => {
-	parent.postMessage({ pluginMessage: { type: 'cancel', body } }, '*');
-};
